cc39e8f4ec5b9898607e8ac85337b9be2b77199c
Seneen Khan <khan.seneen333@gmail.com>
2025-07-11 19:24:50 +0530

    10th program

---
+++ functions.py
@@ -0,0 +1,7 @@
+def new(idea,number): print("Bring up a new" + " "+ idea + " " + number)
+print("Hello everyone")
+new("Dog", "2")
+print("Welcome to the friends list")
+
+def cube(num): return pow(num,3)
+print(cube(5))
