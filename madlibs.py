373559975dd8982bf79badd41a85f1de5b27f88c
Seneen Khan <khan.seneen333@gmail.com>
2025-07-11 17:42:54 +0530

    7th program

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+color = input("What is your favorite color? ")
+plural_noun = input("Shoot a random plural noun: ")
+snack = input("Whats your favorite snack? ")
+print("Roses are " + color)
+print(plural_noun + " " + "are violet")
+print("I love"+ " " + snack)
+
