589bd2a78a4eaf934d0360255e2ffe1037713256
Seneen Khan <khan.seneen333@gmail.com>
2025-07-11 17:02:58 +0530

    4th program

---
+++ numbers.py
@@ -0,0 +1,13 @@
+from math import *
+print(5-2)
+print(10%3)
+my_num = 8
+print(my_num)
+print(str(my_num) + " " +  "times roger won")
+my_num2 = 9
+print(abs(my_num2))
+print(pow(4,9))
+print(round(10.65))
+print(sqrt(9))
+print(floor(34.90))
+print(ceil(34.90))
