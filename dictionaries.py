757c052ec4033bda864921f9259f8b8633041fd8
Seneen Khan <khan.seneen333@gmail.com>
2025-07-12 23:57:02 +0530

    11th program

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+
+
+monthconv = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+print(monthconv.get('Jan'));
