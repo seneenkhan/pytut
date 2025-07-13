5d98b246bb00736d3acb4de18979e04e1303ae5d
Seneen Khan <khan.seneen333@gmail.com>
2025-07-13 16:33:15 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	guessgame.py

---
+++ forloop.py
@@ -0,0 +1,14 @@
+
+
+friends = ["leo", "Dan","jim"]
+for index in range(len(friends)):
+    if index ==0:
+     print('hi')
+    else:
+     print('hello')
+
+
+for friend in friends:
+    if friend == "Dan":
+        break;
+    print(friend)
