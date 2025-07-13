43fa55fe4cc66ac91fd6bd33fb65f96b2383755f
Seneen Khan <khan.seneen333@gmail.com>
2025-07-13 16:22:43 +0530

    12th program

--- guessgame.py
+++ guessgame.py
@@ -1,5 +1,14 @@
 secret_word = "password"
 guess = " "
+count = 0
+limit = 4
+out_of_limit= False
 while guess != secret_word:
-    guess = input("Guess a word: ")
+    if count< limit:
+       guess = input("Guess a word: ")
+    count +=1
+else: out_of_limit = True
+
+       
+
 print("Correct!")
